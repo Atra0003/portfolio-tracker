4a5a3d2653a9ac255d90f257a1db1389c6a81ccc
Amara Traore <amara.traore@ulb.be>
2026-08-28 14:19:51 +0200

    get_current_price and get_current_history

---
+++ src/market_data.py
@@ -0,0 +1,28 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+def get_current_price(ticker):
+    data = yf.Ticker(ticker)
+    todays_data = data.history(period="1d")
+    actual_value = todays_data["Close"].iloc[-1]
+    #print(actual_value)
+
+
+
+def get_price_history(ticker, p):
+    data = yf.Ticker(ticker)
+    DataFrame = data.history(period=p)
+    #data_date_info = DataFrame["Date"]
+    data_close_info = DataFrame["Close"]
+    return data_close_info
+
+
+
+    #DataFrame['Close'].plot()
+    #plt.show()
+
+
+if __name__ == "__main__":
+    ticker = "NQ=F"
+    get_current_price(ticker)
+    print(get_price_history(ticker, '1mo'))
