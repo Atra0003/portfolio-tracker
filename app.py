f47e4993f6f2f8de253ec1b43fd583f5d8cb7e9b
Amara Traore <amara.traore@ulb.be>
2026-08-28 19:01:25 +0200

    mise en place de la db

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from src.market_data import get_current_price, get_price_history
+from src.portfolio import Position
+from src.database import init_db
 
 
 def main():
     ticker = "NQ=F"
     get_current_price(ticker)
-    print(get_price_history(ticker, '1mo'))
+    #print(get_price_history(ticker, '1mo'))
+
+    Portfolio = Position("aaa", 10.0, 100.5, "2000-06-16")
+    init_db()
+
 
 
 
--- src/database.py
+++ src/database.py
@@ -1,19 +1,19 @@
-from dataclasses import dataclass
-from datetime import date
+from src.portfolio import Position
+import sqlite3
 
-@dataclass
-class Position:
-    ticker: str
-    quantite: float
-    prix_achat: float
-    date_achat: date
-    id: int | None = None  # None tant que pas encore en base
 
-    def __post_init__(self):
-        # Validation à la création
-        if self.quantite <= 0:
-            raise ValueError("La quantité doit être positive")
-        if self.prix_achat <= 0:
-            raise ValueError("Le prix d'achat doit être positif")
-        if not self.ticker:
-            raise ValueError("Le ticker ne peut pas être vide")
+def init_db():
+    connexion = sqlite3.connect('data/portfolio.db')
+    cursor = connexion.cursor()
+
+    table = '''CREATE TABLE IF NOT EXISTS portfolio
+                (ticker TEXT, quantite REAL, prix_achat REAL, date_achat TEXT, ID INTEGER PRIMARY KEY)
+                '''
+    cursor.execute(table)
+    #cursor.execute("INSERT INTO portfolio (ticker, quantite, prix_achat, date_achat) VALUES ('AAA', 10, 100, '2026-06-04')")
+    connexion.commit()
+    #cursor.execute("SELECT * FROM portfolio")
+    #result = cursor.fetchall()
+    #print(result)
+
+    connexion.close()
